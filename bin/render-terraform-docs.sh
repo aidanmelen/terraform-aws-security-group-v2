f61c5d76e2300dbf5aa68350ca8d29297410b8cb
aidanmelen <aidan-melen@protonmail.com>
2022-08-26 22:39:46 +0000

    docs

--- bin/render-terraform-docs.sh
+++ bin/render-terraform-docs.sh
@@ -2,10 +2,10 @@
 
 # render terraform-docs code examples
 
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/basic/main.tf > examples/basic/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/complete/main.tf > examples/complete/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/common_rules/main.tf > examples/common_rules/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/custom_rules/main.tf > examples/custom_rules/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/managed_rules/main.tf > examples/managed_rules/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/computed_rules/main.tf > examples/computed_rules/.main.tf.docs
-sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.0"/g' examples/rules_only/main.tf > examples/rules_only/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/basic/main.tf > examples/basic/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/complete/main.tf > examples/complete/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/common_rules/main.tf > examples/common_rules/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/custom_rules/main.tf > examples/custom_rules/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/managed_rules/main.tf > examples/managed_rules/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/computed_rules/main.tf > examples/computed_rules/.main.tf.docs
+sed -z 's/source = [^\r\n]*/source  = "aidanmelen\/security-group-v2\/aws"\n  version = ">= 0.5.1"/g' examples/rules_only/main.tf > examples/rules_only/.main.tf.docs
