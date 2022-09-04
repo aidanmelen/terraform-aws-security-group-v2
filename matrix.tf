d0f0d6c282ae6b972ca8c726b99fa4753b0c8351
aidanmelen <aidan-melen@protonmail.com>
2022-09-04 03:20:12 +0000

    added matrix comments

--- matrix.tf
+++ matrix.tf
@@ -13,8 +13,8 @@ resource "aws_security_group_rule" "computed_matrix_ingress_with_cidr_blocks_and
   cidr_blocks              = try(var.computed_matrix_ingress.cidr_blocks, null)
   ipv6_cidr_blocks         = try(var.computed_matrix_ingress.ipv6_cidr_blocks, null)
   prefix_list_ids          = try(var.computed_matrix_ingress.prefix_list_ids, null)
-  source_security_group_id = null
-  self                     = null
+  source_security_group_id = null # Cannot be specified with cidr_blocks, ipv6_cidr_blocks
+  self                     = null # Cannot be specified with cidr_blocks, ipv6_cidr_blocks
   description              = null
 }
 
@@ -26,11 +26,11 @@ resource "aws_security_group_rule" "computed_matrix_ingress_with_source_security
   from_port                = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["from_port"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["from_port"])
   to_port                  = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["to_port"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["to_port"])
   protocol                 = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["protocol"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["protocol"])
-  cidr_blocks              = null
-  ipv6_cidr_blocks         = null
-  prefix_list_ids          = null
+  cidr_blocks              = null # Cannot be specified with source_security_group_id
+  ipv6_cidr_blocks         = null # Cannot be specified with source_security_group_id
+  prefix_list_ids          = null # Cannot be specified with source_security_group_id
   source_security_group_id = var.computed_matrix_ingress.source_security_group_ids[count.index % length(var.computed_matrix_ingress["rules"])]
-  self                     = null
+  self                     = null # Cannot be specified with source_security_group_id
   description              = null
 }
 
@@ -42,10 +42,10 @@ resource "aws_security_group_rule" "computed_matrix_ingress_with_self" {
   from_port                = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["from_port"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["from_port"])
   to_port                  = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["to_port"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["to_port"])
   protocol                 = try(local.rules[var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["rule"]]["protocol"], var.computed_matrix_ingress.rules[count.index % length(var.computed_matrix_ingress["rules"])]["protocol"])
-  cidr_blocks              = null
-  ipv6_cidr_blocks         = null
-  prefix_list_ids          = null
-  source_security_group_id = null
+  cidr_blocks              = null # Cannot be specified with self
+  ipv6_cidr_blocks         = null # Cannot be specified with self
+  prefix_list_ids          = null # Cannot be specified with self
+  source_security_group_id = null # Cannot be specified with self
   self                     = true
   description              = null
 }
@@ -65,8 +65,8 @@ resource "aws_security_group_rule" "computed_matrix_egress_with_cidr_blocks_and_
   cidr_blocks              = try(var.computed_matrix_egress.cidr_blocks, null)
   ipv6_cidr_blocks         = try(var.computed_matrix_egress.ipv6_cidr_blocks, null)
   prefix_list_ids          = try(var.computed_matrix_egress.prefix_list_ids, null)
-  source_security_group_id = null
-  self                     = null
+  source_security_group_id = null # Cannot be specified with cidr_blocks, ipv6_cidr_blocks
+  self                     = null # Cannot be specified with cidr_blocks, ipv6_cidr_blocks
   description              = null
 }
 
@@ -78,11 +78,11 @@ resource "aws_security_group_rule" "computed_matrix_egress_with_source_security_
   from_port                = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["from_port"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["from_port"])
   to_port                  = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["to_port"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["to_port"])
   protocol                 = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["protocol"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["protocol"])
-  cidr_blocks              = null
-  ipv6_cidr_blocks         = null
-  prefix_list_ids          = null
+  cidr_blocks              = null # Cannot be specified with source_security_group_id
+  ipv6_cidr_blocks         = null # Cannot be specified with source_security_group_id
+  prefix_list_ids          = null # Cannot be specified with source_security_group_id
   source_security_group_id = var.computed_matrix_egress.source_security_group_ids[count.index % length(var.computed_matrix_egress["rules"])]
-  self                     = null
+  self                     = null # Cannot be specified with source_security_group_id
   description              = null
 }
 
@@ -94,10 +94,10 @@ resource "aws_security_group_rule" "computed_matrix_egress_with_self" {
   from_port                = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["from_port"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["from_port"])
   to_port                  = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["to_port"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["to_port"])
   protocol                 = try(local.rules[var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["rule"]]["protocol"], var.computed_matrix_egress.rules[count.index % length(var.computed_matrix_egress["rules"])]["protocol"])
-  cidr_blocks              = null
-  ipv6_cidr_blocks         = null
-  prefix_list_ids          = null
-  source_security_group_id = null
+  cidr_blocks              = null # Cannot be specified with self
+  ipv6_cidr_blocks         = null # Cannot be specified with self
+  prefix_list_ids          = null # Cannot be specified with self
+  source_security_group_id = null # Cannot be specified with self
   self                     = true
   description              = null
 }
